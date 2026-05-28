5419b171a938e69607bb691203c9c96403814b55
RAUNIfied <Piyushrauniyar16@gmail.com>
2026-05-28 18:46:54 +0545

    fix: add production domains to CORS whitelist

--- server/src/app.js
+++ server/src/app.js
@@ -49,6 +49,8 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:3000",
+      "http://98.70.55.34",
+      "https://98.70.55.34.nip.io",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
